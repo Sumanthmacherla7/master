4ecd93af81a7a8bb6380205af7895f42c42b6375
Sumanthmacherla7 <115871256+Sumanthmacherla7@users.noreply.github.com>
2023-04-03 17:37:59 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
-    // Prints ("Hello, World");
+    // Prints "Hello, World" in the terminal window.
+    system.out.println("Hello, World");
   }
 }
   
